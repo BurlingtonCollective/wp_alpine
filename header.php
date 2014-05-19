b7f4d026d71131d1348c8a7a4f0625c716cfb32f
Zachary Lincoln <zach@bluehousegroup.com>
2014-05-19 17:04:33 -0400

    starting building around wireframes

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Home | Alpine Amusement</title>
+	<link href="<?= get_template_directory_uri(); ?>/css/master.css" rel="stylesheet">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script type="text/javascript" src="<?= get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default" role="navigation">
+	  <div class="container">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="index.html">Brand</a>
+	    </div>
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="index.html">Home</a></li>
+	        <li><a href="galleries.html">Galleries</a></li>
+	        <li><a href="testimonials.html">Testimonials</a></li>
+	        <li><a href="faq.html">FAQ</a></li>
+	        <li><a href="contact_us.html">Contact Us</a></li>
+		  </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
---
+++ home.php
@@ -0,0 +1,140 @@
+<?php get_header(); ?>
+<section id="home-banner">
+	<div class="jumbotron">
+		<div class="container">
+			<div class="row banner-top">
+				<div class="col-xs-12">
+					<h1>Alpine Amusement</h1>
+					<h3>Casino themed parties available throughout New England</h3>
+					<div class="btn-group btn-group-lg">
+						<a class="btn btn-default" href="tel:+18888888888">(888) 888-8888</a>
+						<a class="btn btn-primary" href="contact_us.html">Contact Us</a>
+					</div>
+				</div>
+			</div>
+			<div class="row banner-bottom">
+				<div class="col-xs-12">
+					<div class="testimonial-slider">
+						<div class="testimonial">
+							<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+							  <!-- Indicators -->
+							  <ol class="carousel-indicators">
+							    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+							    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+							    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+							  </ol>
+
+							  <!-- Wrapper for slides -->
+							  <div class="carousel-inner">
+							    <div class="item active">
+							    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							    </div>
+							    <div class="item">
+							    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							    </div>
+							    <div class="item">
+							    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							    </div>
+							    <div class="item">
+							    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							    </div>
+							  </div>
+
+							  <!-- Controls -->
+							  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+							    <span class="glyphicon glyphicon-chevron-left"></span>
+							  </a>
+							  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+							    <span class="glyphicon glyphicon-chevron-right"></span>
+							  </a>
+							</div>
+						</div>
+						<div class="logo-bar">
+							<div class="row">
+								<div class="col-xs-2">
+									<img class="img-responsive" src="http://placehold.it/200x100"/>
+								</div>
+								<div class="col-xs-2">
+									<img class="img-responsive" src="http://placehold.it/200x100"/>
+								</div>
+								<div class="col-xs-2">
+									<img class="img-responsive" src="http://placehold.it/200x100"/>
+								</div>
+								<div class="col-xs-2">
+									<img class="img-responsive" src="http://placehold.it/200x100"/>
+								</div>
+								<div class="col-xs-2">
+									<img class="img-responsive" src="http://placehold.it/200x100"/>
+								</div>
+								<div class="col-xs-2">
+									<img class="img-responsive" src="http://placehold.it/200x100"/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+	<div class= "container">
+		<div class="row">
+			<div class="col-xs-5">
+				<h4>Lorem ipsum dolor sit amet</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>We offer services for all types of events:</p>
+				<ul>
+					<li>Corporate Parties</li>
+					<li>Office Parties</li>
+					<li>Company Events</li>
+					<li>Team Building</li>
+					<li>Fundraisers</li>
+					<li>Weddings</li>
+					<li>Holiday Parties</li>
+					<li>Family Gatherings &amp; Reunions</li>
+        </ul>
+			</div>
+			<div class="col-xs-6 col-xs-push-1">
+				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				  </ol>
+
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner">
+				    <div class="item active">
+				    	<img class="img-responsive" src="http://placehold.it/800x400">
+				    </div>
+				    <div class="item">
+				    	<img class="img-responsive" src="http://placehold.it/800x400">
+				    </div>
+				    <div class="item">
+				    	<img class="img-responsive" src="http://placehold.it/800x400">
+				    </div>
+				    <div class="item">
+				    	<img class="img-responsive" src="http://placehold.it/800x400">
+				    </div>
+				  </div>
+
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left"></span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right"></span>
+				  </a>
+				</div>
+			</div>
+		</div>		
+	</div>
+</section>
+<?php get_footer(); ?>
