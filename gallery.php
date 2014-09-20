dd91782cd2c0b73017cb8268d5a2bd3b71b4c2ef
Zachary Lincoln <zach@burlingtoncollective.com>
2014-09-20 18:39:05 -0400

    gallery page + styling + scans

---
+++ gallery.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Template Name: Gallery Page
+*/
+get_header();
+?>
+<section id="gallery">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="page-header">
+					<h1>Gallery</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container interior">
+		<div class="row">
+			<div class="col-xs-12">
+				<h2 id="special-occasions">Special Occasions</h2>
+			</div>
+		</div>
+		<div class="row images">
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/special/1.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/special/2.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/special/3.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/special/4.jpeg">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<h2 id="private-parties">Private Parties</h2>
+			</div>
+		</div>
+		<div class="row images">
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/private/1.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/private/2.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/private/3.jpeg">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<h2 id="theme-parties">Theme Parties</h2>
+			</div>
+		</div>
+		<div class="row images">
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/theme/1.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/theme/2.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/theme/3.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/theme/4.jpeg">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<h2 id="non-profits">Non-Profits</h2>
+			</div>
+		</div>
+		<div class="row images">
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/non-profits/1.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/non-profits/2.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/non-profits/3.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/non-profits/4.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/non-profits/5.jpeg">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<h2 id="outdoor">Tents/Outdoor Venues</h2>
+			</div>
+		</div>
+		<div class="row images">
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/tents/1.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/tents/2.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/tents/3.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/tents/4.jpeg">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<h2 id="corporate">Corporate Parties</h2>
+			</div>
+		</div>
+		<div class="row images">
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/corporate/1.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/corporate/2.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/corporate/3.jpeg">
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/corporate/4.jpeg">
+			</div>
+		</div>
+	</div>
+</section>
+<?php
+get_footer();
+?>
