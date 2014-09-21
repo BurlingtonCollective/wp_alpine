4e6b565c999092e695d7e19fe079fbb1cd5d56b7
Zachary Lincoln <zach@burlingtoncollective.com>
2014-09-21 16:36:07 -0400

    swapped out wrong orientation

--- gallery.php
+++ gallery.php
@@ -82,7 +82,7 @@ get_header();
 				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/non-profits/1.jpeg">
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-4">
-				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/non-profits/2.jpeg">
+				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/non-profits/2.JPG">
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-4">
 				<img class="img-responsive" src="<?= get_template_directory_uri();?>/images/scans/non-profits/3.jpeg">
